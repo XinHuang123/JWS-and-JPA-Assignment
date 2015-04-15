9307350a57381901efc69c7ebc250426197b8be6
XinHuang123 <Huang.xin1@husky.neu.edu>
2015-04-14 20:07:36 -0400

    Homework5

--- src/edu/neu/cs5200/jpa/orm/dao/SiteDAO.java
+++ src/edu/neu/cs5200/jpa/orm/dao/SiteDAO.java
@@ -1,12 +1,17 @@
 package edu.neu.cs5200.jpa.orm.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -21,11 +26,20 @@ public class SiteDAO {
 	EntityManager em = factory.createEntityManager();
 	
 		// createSite
-		public Site createSite(Site site) {
+	    @POST
+	    @Path("/site")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    @Produces(MediaType.APPLICATION_JSON)	
+		public List<Site> createSite(Site site) {
+			List<Site> sites=new ArrayList<Site>();
+			
 			em.getTransaction().begin();
 			em.persist(site);
+			Query query=em.createNamedQuery("findAllSites");
+			sites=query.getResultList();
 			em.getTransaction().commit();
-			return site;
+			em.close();
+			return sites;
 		}
 		
 
@@ -38,7 +52,7 @@ public class SiteDAO {
 		{
 			return em.find(Site.class, id);
 		}
-		
+			
 		// readAllMovies
 		@GET
 		@Path("/site")
@@ -50,27 +64,46 @@ public class SiteDAO {
 		}
 		
 		// updateMovie
-		public Site updateSite(Site site)
+		@PUT
+		@Path("/site/ID/{id}")
+		@Consumes(MediaType.APPLICATION_JSON)
+		@Produces(MediaType.APPLICATION_JSON)
+		public List<Site> updateSite(@PathParam("id") Integer siteid, Site site)
 		{
+			List<Site> sites=new ArrayList<Site>();
 			em.getTransaction().begin();
+			site.setId(siteid);
 			em.merge(site);
+			Query query=em.createNamedQuery("findAllSites");
+			sites=query.getResultList();
 			em.getTransaction().commit();
-			return site;
+			em.close();
+			return sites;
 		}
+		
 		// deleteMovie
-		public void  removeSite(int id) {
+		@DELETE
+		@Path("/site/ID/{id}")
+		@Produces(MediaType.APPLICATION_JSON)
+		public List<Site> removeSite(@PathParam("id") int id) {
+			List<Site> sites=new ArrayList<Site>();
+			Site site=null;
 			em.getTransaction().begin();
-			Site site = em.find(Site.class, id);
+			site = em.find(Site.class, id);
 			em.remove(site);
+			Query query=em.createNamedQuery("findAllSites");
+			sites=query.getResultList();
 			em.getTransaction().commit();
+			em.close();
+			return sites;
 		}
 
-	
+		/*
 	public static void main(String args[]){
 		SiteDAO dao=new SiteDAO();		
 		dao.removeSite(3);
 		
 		
 	}
-
+	*/
 }
